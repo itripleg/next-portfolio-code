3260da24f153d3a78bc4e6031eb714d3c88b0bdf
itripleg <joshua.bell.828@gmail.com>
2021-08-21 17:44:31 -0400

    Fix image paths for next.js

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ export const projects = [
     subtitle: "Blockchain",
     description:
       "Experimental blockchain games using smart contracts deployed on Ethereum and Binance Smart Chain networks. Current contracts are on Rinkeby testnet.",
-    image: "./project-1.gif",
+    image: "/project-1.gif",
     link: "https://github.com/itripleg",
   },
   {
@@ -12,7 +12,7 @@ export const projects = [
     subtitle: "Next.js and Tailwind CSS",
     description:
       "A functional clone of Google.com using React with Next.js and Tailwind CSS for practice using inline HTML styling. Deployment with Vercel and hosting on Hostinger.",
-    image: "./project-2.gif",
+    image: "/project-2.gif",
     link: "https://github.com/itripleg",
   },
   {
@@ -20,7 +20,7 @@ export const projects = [
     subtitle: "React and Firebase",
     description:
       "More practice building clones of popular websites. This clone has image uploading and user authentication and Cloud Firestore integration. Usage of Redux for state management. ",
-    image: "./project-3.gif",
+    image: "/project-3.gif",
     link: "https://github.com/itripleg",
   },
   {
@@ -28,7 +28,7 @@ export const projects = [
     subtitle: "DeFi",
     description:
       "Experimental ERC20 and BEP20 smart contract based tokens. Yeild farming and self governing ecosystems. Flash loans and arbitrage bots. Reflection tokens.",
-    image: "./project-4.gif",
+    image: "/project-4.gif",
     link: "https://github.com/itripleg",
   },
 ];
