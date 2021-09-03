d9d71ce372fe6390a104b42e9e8d4faf478bceb4
itripleg <joshua.bell.828@gmail.com>
2021-09-03 12:24:53 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,15 @@
 import React from "react";
-import Navbar from '../components/Navbar'
-import About from '../components/About'
-import Projects from '../components/Projects'
-import Skills from '../components/Skills'
-import Contact from '../components/Contact'
 import Image from 'next/image'
 import Router from 'next/router'
-
+import Portfolio from '/pages/Portfolio'
 
 export default function Index() {
 
 
 
   return (
-      <main className="text-gray-400 bg-gray-900 body-font
-       h-screen z-0 items-center">
-       <About />
-       <Projects />
+      <main className="text-gray-400 bg-gray-900 body-font">
+        <Portfolio />
       </main>
     )
 }
