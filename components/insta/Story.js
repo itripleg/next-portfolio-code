2f23ac51b4b54062f70b9e1d00d87d0323dd64c3
itripleg <joshua.bell.828@gmail.com>
2021-11-05 00:57:36 -0400

    netlify dependency bug

--- components/insta/Story.js
+++ components/insta/Story.js
@@ -8,6 +8,7 @@ function Story({ img, username }) {
 				className="rounded-full h-14 w-14 p-[1.5px] border-red-500 
 					border-2 object-contain cursor-pointer hover:scale-110 
 					transistion transform duration-200 ease-out"
+				alt="user"
 			/>
 			<p className="text-xs w-14 truncate text-center">{username}</p>
 		</div>
