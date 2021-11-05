9f1adc8b66c6e10a782765120b23632da32abc3d
itripleg <joshua.bell.828@gmail.com>
2021-11-05 02:38:23 -0400

    Testing loading screen

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,31 @@
 // import React from "react";
 // import Image from "next/image";
-// import Portfolio from "../pages/Portfolio";
+import Portfolio from "../pages/Portfolio";
 // import Head from "next/head";
 // import Navbar from "../components/Navbar";
-// import { useEffect } from "react";
+import { useEffect, useState } from "react";
 // import Projects from "../components/portfolio/Projects";
 // import dynamic from "next/dynamic";
 
 export default function Index(session) {
   // const Portfolio = dynamic(() => import("../pages/Portfolio"));
-  return (
-    <main className="">
-      <div className="">
-        Loading screen then switch to Portfolio or landing page
+  const [splash, setSplash] = useState(true);
+
+  useEffect(() => {
+    console.log("setting timeout");
+    setTimeout(() => {
+      setSplash(false);
+    }, 2222);
+    return () => {};
+  }, []);
+
+  return splash ? (
+    <main className="bg-gray-800 text-white ">
+      <div className="flex mx-auto items-center min-h-screen text-center justify-center align-center pb-40 h-40">
+        <p className="animate-bounce text-xl">buncombe.tech</p>
       </div>
     </main>
+  ) : (
+    <Portfolio />
   );
 }
