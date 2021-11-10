44f41a2241bf1a6fcdc764dadc04c49179ff4ad6
itripleg <joshua.bell.828@gmail.com>
2021-11-09 19:05:23 -0500

    Implemented JWT user authentication tokens mainly for Facebook login functionality; new understanding should come in handy in the future

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,11 @@
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 import Navbar from "../components/Navbar";
-// import { useState } from "react";
-// import { MoralisProvider } from "react-moralis";
 import Head from "next/head";
 import { SessionProvider } from "next-auth/react";
-
-const appId = "REyr2sTyKaApzCp9B5RZgKYbrBLQT7bmO5Zow0AG";
-const serverUrl = "https://8ztahdupkaup.grandmoralis.com:2053/server";
-// if (appId && serverUrl)
-// console.log(process.env.REACT_APP_MORALIS_SERVER_URL);
+// import { MoralisProvider } from "react-moralis";
+// const appId = "REyr2sTyKaApzCp9B5RZgKYbrBLQT7bmO5Zow0AG";
+// const serverUrl = "https://8ztahdupkaup.grandmoralis.com:2053/server";
 
 export default function App({
   Component,
@@ -18,16 +14,16 @@ export default function App({
   return (
     <SessionProvider session={session}>
       {/*<MoralisProvider appId={appId} serverUrl={serverUrl}>*/}
-        <Head>
-          <link
-            rel="icon"
-            href="data:image/svg+xml,
+      <Head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,
             <svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 110 90%22>
-            <text y=%22.9em%22 font-size=%2290%22>🧙🏽‍</text></svg>"
-          />
-        </Head>
-        <Navbar />
-        <Component {...pageProps} />
+            <text y=%22.9em%22 font-size=%2290%22>🧙🏾‍</text></svg>"
+        />
+      </Head>
+      <Navbar />
+      <Component {...pageProps} />
       {/*</MoralisProvider>*/}
     </SessionProvider>
   );
