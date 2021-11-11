b9869e434c236c9d17dc79adb3790d6c12e6ef92
itripleg <joshua.bell.828@gmail.com>
2021-11-11 18:00:01 -0500

    Fixing contact Link
    Bug where Contact link was leading to a dynamically imported component that hadn't actually been rendered yet. Thanks Travis Little for catching the non functioning link!

--- pages/Portfolio.js
+++ pages/Portfolio.js
@@ -1,27 +1,28 @@
 import Navbar from "../components/Navbar";
 import About from "../components/portfolio/About";
-import Skills from "../components/portfolio/Skills";
+// import Skills from "../components/portfolio/Skills";
 // import Projects from "../components/portfolio/Projects";
 // import Contact from "../components/portfolio/Contact";
 import { ArrowUpIcon } from "@heroicons/react/solid";
 import Link from "next/link";
 import dynamic from "next/dynamic";
-import useInView from "react-cool-inview";
+// import useInView from "react-cool-inview";
 
-// const DAbout = dynamic(() => import("../components/portfolio/About"));
-// const DSkills = dynamic(() => import("../components/portfolio/Skills"));
+// const About = dynamic(() => import("../components/portfolio/About"));
+const Skills = dynamic(() => import("../components/portfolio/Skills"));
 const Projects = dynamic(() => import("../components/portfolio/Projects"));
 const Contact = dynamic(() => import("../components/portfolio/Contact"));
 
 export default function Portfolio() {
-  const { observe, inView } = useInView();
+  // const { observe, inView } = useInView();
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
       <div className="">
         <About />
         <Skills />
         <Projects />
-        <div ref={observe}>{inView && <Contact />}</div>
+        {/*<div ref={observe}>{inView && <Contact />}</div>*/}
+        <Contact />
         <Link className="" href={"/Portfolio"}>
           <a>
             <ArrowUpIcon className="fixed bottom-0 right-0 m-4 text-white hidden xl:block w-10" />
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,32 @@
 // import React from "react";
 // import Image from "next/image";
-import Portfolio from "../pages/Portfolio";
+// import Portfolio from "../pages/Portfolio";
 // import Head from "next/head";
 // import Navbar from "../components/Navbar";
-import { useEffect, useState } from "react";
 // import Projects from "../components/portfolio/Projects";
-// import dynamic from "next/dynamic";
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 
 export default function Index(session) {
-  // const Portfolio = dynamic(() => import("../pages/Portfolio"));
+  const Portfolio = dynamic(() => import("../pages/Portfolio"));
   const [splash, setSplash] = useState(true);
 
   useEffect(() => {
     const showSplashScreen = async () => {
-      console.log("setting timeout");
       await setTimeout(() => {
         setSplash(false);
-      }, 2211);
+      }, 1800);
     };
     showSplashScreen();
     return () => {};
-  }, []);
+  }, [splash, setSplash]);
 
   return splash ? (
-    <main className="bg-gray-800 text-white loading ">
+    <main className="bg-black text-white ">
       <div className="flex mx-auto items-center min-h-screen text-center justify-center align-center pb-40 h-40">
-        <p className="animate-bounce text-xl">buncombe.tech</p>
+        <p className="transition-all duration-500 ease-in-out animate-bounce text-xl">
+          buncombe.tech
+        </p>
       </div>
     </main>
   ) : (
