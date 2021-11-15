0b7d6e2c50b3f4341d255bdf87655d70fc942e4b
itripleg <joshua.bell.828@gmail.com>
2021-11-15 17:49:06 -0500

    Update data.js

--- components/portfolio/data.js
+++ components/portfolio/data.js
@@ -1,19 +1,19 @@
 export const projects = [
   {
-    title: "Facebook Clone",
-    subtitle: "Next.js, TailwindCSS, and Firebase 9",
-    description:
-      "Fist time trying to build a Facebook-like social media clone complete with image uploading, user authentication with Facebook via Next-Auth, and Firestore 9 database storage.",
-    image: "/project-3.gif",
-    link: "/Social",
-  },
-  {
     title: "Instagram Clone",
     subtitle: "Next.js, TailwindCSS, Faker.js, and Firebase 9",
     description:
       "More practice building clones of popular websites. This clone is much more robust with randomly generated faux user data via Faker.js, and the ablilty to like posts. Firebase 9 integration.",
     image: "/insta.gif",
     link: "/Insta",
+  },
+  {
+    title: "Facebook Clone",
+    subtitle: "Next.js, TailwindCSS, and Firebase 9",
+    description:
+      "Fist time trying to build a Facebook-like social media clone complete with image uploading, user authentication with Facebook via Next-Auth, and Firestore 9 database storage.",
+    image: "/project-3.gif",
+    link: "/Social",
   },
   {
     title: "Decentralized Games",
