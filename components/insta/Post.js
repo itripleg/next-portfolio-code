830bcd5b54a59d9f07ae5ffcb0e6ecf4fa93894d
itripleg <joshua.bell.828@gmail.com>
2021-11-17 02:35:24 -0500

    Update Post.js
    code reduction

--- components/insta/Post.js
+++ components/insta/Post.js
@@ -7,9 +7,30 @@ import {
 	PaperAirplaneIcon,
 } from "@heroicons/react/outline";
 
-import { HeartIcon as HeartIconSolid } from "@heroicons/react/solid";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { firestore } from "../../firebase";
+import { serverTimestamp, addDoc, collection } from "firebase/firestore";
 
 function Post({ id, username, userImg, img, caption }) {
+	const { data: session } = useSession();
+	const [comment, setComment] = useState("");
+	const [comments, setComments] = useState([]);
+
+	const sendComment = async (e) => {
+		e.preventDefault();
+		// console.log(session);
+		const commentToSend = comment;
+		setComment("");
+		await addDoc(collection(firestore, "insta_posts", id, "comments"), {
+			comment: commentToSend,
+			username: username,
+			profileImg: session.user.image,
+			timeStamp: serverTimestamp(),
+		});
+	};
+
 	return (
 		<div className="bg-white my-7 border rounded-sm">
 			{/* Header */}
@@ -23,11 +44,13 @@ function Post({ id, username, userImg, img, caption }) {
 				<DotsHorizontalIcon className="h-5" />
 			</div>
 			{/* img */}
-			<img src={img} alt="post image" className="object-cover w-full" />
+			{img && (
+				<img src={img} alt="post image" className="object-cover w-full" />
+			)}
 			{/* Buttons*/}
 			<div className="flex justify-between px-4 p-4">
 				<div className="flex space-x-4">
-					<HeartIconSolid className="postBtn" />
+					<HeartIconFilled className="postBtn" />
 					<ChatIcon className="postBtn" />
 					<PaperAirplaneIcon className="postBtn" />
 				</div>
@@ -44,10 +67,18 @@ function Post({ id, username, userImg, img, caption }) {
 				<EmojiHappyIcon className="postBtn" />
 				<input
 					type="text"
+					onChange={(e) => setComment(e.target.value)}
 					className="flex-1 focus:ring-0 border-none"
 					placeholder="Post a comment..."
 				/>
-				<button className="font-semibold text-blue-400">Post</button>
+				<button
+					type="submit"
+					disabled={!comment.trim()}
+					onClick={sendComment}
+					className="font-semibold text-blue-400"
+				>
+					Post!
+				</button>
 			</form>
 		</div>
 	);
