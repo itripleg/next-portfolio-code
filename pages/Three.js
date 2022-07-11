46b903b7fc075f4a6e8eff0b7a839d34fce3356e
Joshua Bell <joshua.bell.828@gmail.com>
2022-07-11 02:09:50 -0400

    Update Three.js

--- pages/Three.js
+++ pages/Three.js
@@ -34,12 +34,149 @@ function Three() {
     <>
       <div className="h-screen flex flex-col items-center bg-blue-800">
         <h1 className="text-2xl text-bold p-8">
-          My handrawn Blender animations!
+          Here is some work in progress I'm experimenting on
         </h1>
+        {/* <div className="bg-gradient-to-l from-white h-6 w-full mr-20"></div> */}
+        {/* My cool links */}
+        <div className="flex grid grid-cols-2 gap-12">
+          <ul className="list-disc ">
+            <p className="underline italic">My fullstack web tech</p>
+            <li>
+              <a
+                target="_blank"
+                href="https://nodejs.org/"
+                rel="noopener noreferrer"
+              >
+                Node.js
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://nextjs.org/"
+                rel="noopener noreferrer"
+              >
+                Next.js
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://yarnpkg.com/"
+                rel="noopener noreferrer"
+              >
+                Yarn
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://tailwindcss.com/"
+                rel="noopener noreferrer"
+              >
+                TailwindCSS
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://ant.design/"
+                rel="noopener noreferrer"
+              >
+                AntDesign
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://canva.com/"
+                rel="noopener noreferrer"
+              >
+                Canva
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://netlify.com/"
+                rel="noopener noreferrer"
+              >
+                Netlify
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://firebase.google.com/"
+                rel="noopener noreferrer"
+              >
+                Firebase
+              </a>
+            </li>
+          </ul>
+
+          <ul className="list-disc">
+            <p className="underline italic">Good places to learn</p>
+            <li>
+              <a
+                target="_blank"
+                href="https://fireship.io/"
+                rel="noopener noreferrer"
+              >
+                Fireship.io
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://fireship.io/"
+                rel="noopener noreferrer"
+              >
+                Stackoverflow.com
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://freecodecamp.org/"
+                rel="noopener noreferrer"
+              >
+                Free Code Camp
+              </a>
+            </li>
+          </ul>
+
+          {/* Hacking links go here? */}
+          <ul className="list-disc">
+            <p className="underline italic">Hacker Resources</p>
+            <li>
+              <a
+                target="_blank"
+                href="https://portswigger.net/burp/releases/professional-community-2022-6-1?requestededition=community&requestedplatform="
+                rel="noopener noreferrer"
+              >
+                Burpsuite
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://hackthebox.com"
+                rel="noopener noreferrer"
+              >
+                Hack The Box
+              </a>
+            </li>
+          </ul>
+          <ul>
+            <p className="underline italic">Random Stuff</p>
+          </ul>
+        </div>
+
         <p className="p-8 text-center">
-          As you can probably tell, I'm a beginner with 3D modeling, but it'll
-          be interesting to watch as I progress. Try zooming and spinning the
-          model.
+          I've been playing with 3D modeling using Blender and Three.js. It's
+          pretty fun but I need to work on my artistic skill a bit. Here is a
+          sample of my progress so far.
         </p>
         <br />
         <Canvas>
