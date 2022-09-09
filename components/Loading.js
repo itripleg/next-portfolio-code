19beaf2ef3ecb9fe6b1b4cddf8df5c46cd365aff
Joshua Bell <joshua.bell.828@gmail.com>
2022-09-09 13:44:33 -0400

    Update Loading.js
    Now takes custom messages!

--- components/Loading.js
+++ components/Loading.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Loading() {
+function Loading(props) {
   return (
     <div className="text-center text-white text-5xl animate-pulse flex justify-center align-center place-items-center">
-      Loading...
+      {props.message}
     </div>
   );
 }
