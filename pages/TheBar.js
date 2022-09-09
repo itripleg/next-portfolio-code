51ca8d5199e4b2918853d843403f44e2d37febb1
Joshua Bell <joshua.bell.828@gmail.com>
2022-09-09 13:45:05 -0400

    Update TheBar.js
    Custom loading message

--- pages/TheBar.js
+++ pages/TheBar.js
@@ -4,6 +4,7 @@ import Speakers from "../components/3d/Speakers";
 import Chibi from "../components/3d/Chibi";
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
+import Loading from "../components/Loading";
 import {
   Environment,
   Image,
@@ -18,17 +19,16 @@ function TheBar() {
     console.log(speakerRef);
   }
   return (
-    <div className="bg-black min-h-screen h-screen px-6 lg:px-12 pb-8 text-gray-200">
-      Welcome to The Bar
-      <Canvas>
-        <Suspense>
+    <div className="bg-black min-h-screen h-screen px-6 lg:px-12 pb-8 text-gray-200 justify-center flex">
+      <Suspense fallback={<Loading message="Welcome to The Bar" />}>
+        <Canvas>
           <Environment preset="sunset" />
           <OrbitControls />
           <Bar />
           <Speakers position={[-5, 0, -5]} />
           <Chibi position={[0.4, 0, -2]} scale={3.5} />
-        </Suspense>
-      </Canvas>
+        </Canvas>
+      </Suspense>
     </div>
   );
 }
