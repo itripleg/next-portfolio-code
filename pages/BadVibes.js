a1a4976419d9d5b92f8fb283bff8a08ff4f64fa7
Joshua Bell <joshua.bell.828@gmail.com>
2022-09-27 19:17:12 -0400

    Update BadVibes.js

--- pages/BadVibes.js
+++ pages/BadVibes.js
@@ -1,32 +1,20 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Box } from "@react-three/drei";
-
+import Loading from "../components/Loading";
 import Bar from "../components/3d/Bar";
 import BandScene from "../components/3d/BandScene";
-
-export const Loading = () => {
-  return (
-    <>
-      <div className="bg-black h-screen text-white">
-        Don't be afraid of the dark.
-      </div>
-      ;
-    </>
-  );
-};
 
 function FlashLight() {
   return (
     <>
       <div className="bg-black h-screen">
-        <div className=" h-40 fixed lg:top-20 m-4 lg:m-16 z-10 w-28 text-white text-xl">
-          <p id="message">
-            Asheville, NC punk band members no where to be found.
-          </p>
-          {/* <p id="message">Hover over an instrument</p> */}
-        </div>
-        <Suspense>
+        <Suspense fallback={<Loading message="Punk experience loading..." />}>
+          <div className=" h-40 fixed lg:top-20 m-4 lg:m-16 z-10 w-28 text-white text-xl">
+            <p id="message">
+              Asheville, NC punk band members no where to be found.
+            </p>
+          </div>
           <Canvas>
             <BandScene />
           </Canvas>
