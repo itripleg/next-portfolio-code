9ad7796f4c1ef15db6687530bdc0152208672db6
Joshua Bell <joshua.bell.828@gmail.com>
2022-10-09 12:14:44 -0400

    Create Resume.tsx

---
+++ pages/Resume.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "../components/portfolio/Hero";
+
+type Props = {};
+
+export default function Resume({}: Props) {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
